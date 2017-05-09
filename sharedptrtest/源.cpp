428acb7dd18ac6fa4d4b1003534199198b4f992e
Nov11 <529079634@qq.com>
2017-05-09 17:43:39 +0800

    multi enable_shared_from_this

--- sharedptrtest/源.cpp
+++ sharedptrtest/源.cpp
@@ -1,6 +1,22 @@
 #include <boost/shared_ptr.hpp>
 #include <vector>
 #include <iostream>
+#include "../boost_1_62_0/boost/smart_ptr/enable_shared_from_this.hpp"
+
+
+using namespace boost;
+
+class A {
+
+};
+class B :public enable_shared_from_this<B> , public A{};
+class C :public B{};
+//if we place c in the end, this works just fine
+class D : public C, public enable_shared_from_this<D> {
+public:
+	D(int i){}
+	boost::shared_ptr<D> fun() { return boost::enable_shared_from_this<D>::shared_from_this(); }
+};
 
 int main()
 {
@@ -16,4 +32,9 @@ int main()
 	std::cout << (v[0] == ptr) << std::endl;
 	std::cout << (v[1] == ptr) << std::endl;
 	//std::cout << (v[0].get() == ptr.get()) << std::endl;
+
+	std::cout << "======================" << std::endl;
+	boost::shared_ptr<D> stmptr(new D(12));
+	D* pd = stmptr.get();
+	pd->fun();
 }
