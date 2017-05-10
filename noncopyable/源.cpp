77466a60ec5c629031cd27b6dabb2204a5eec752
Nov11 <529079634@qq.com>
2017-05-10 18:22:22 +0800

    trials

--- noncopyable/源.cpp
+++ noncopyable/源.cpp
@@ -1,4 +1,7 @@
 #include <boost/noncopyable.hpp>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 
 
 class A : boost::noncopyable{};
@@ -6,6 +9,11 @@ class B : A {};
 
 int main()
 {
-	A a;
-	B b;
+	//A a;
+	//B b;
+	std::vector<int> v{ 1,2,3,4,5,6 };
+	int i = 4;
+	auto ret = std::find_if(v.begin(), v.end(), [&](int param)->bool {return param == i; });
+	std::cout << (ret != v.end()) << std::endl;
+	std::cout << *ret << std::endl;
 }
